bcc6d3342063f229e8c1975b1e1c94b82c45af19
Daniel Yo <daniel.yo@neudesic.com>
2022-06-14 14:50:29 -0700

    add navigation and table

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react';
+
+export const HomeScreen = () => {
+  return (
+    <TableContainer>
+      <Table variant="striped" colorScheme="teal">
+        <TableCaption>Dad joke list</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Id</Th>
+            <Th>Joke</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>1</Td>
+            <Td>Test 1</Td>
+          </Tr>
+          <Tr>
+            <Td>2</Td>
+            <Td>Test 2</Td>
+          </Tr>
+          <Tr>
+            <Td>3</Td>
+            <Td>Test 3</Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
